b7c125f5233cc86566ce58a8b25889c6c11a562b
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-07-25 12:52:06 +0300

    update

--- src/js/02-feedback.js
+++ src/js/02-feedback.js
@@ -1,6 +1,40 @@
 import '../css/common.css';
 import '../css/feedback-form.css';
+import { loadFromLS, saveToLS } from './helpers.js';
 
+// посилання
 const refs = {
   form: document.querySelector('.js-feedback-form'),
 };
+
+// прослуховувач інпут події
+refs.form.addEventListener('input', e => {
+  const key = e.target.name;
+  const value = e.target.value;
+  saveToLS(key, value);
+});
+
+// ================================= //
+// Прослуховування сабміту
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const name = e.target.elements.name.value;
+  const message = e.target.elements.message.value;
+
+  console.log({ name, message });
+
+  localStorage.clear();
+  e.target.reset();
+});
+// ================================= //
+
+// при першому завантаженні виконати цю функцію
+function onPageLoad() {
+  const name = loadFromLS('name');
+  const message = loadFromLS('message');
+
+  refs.form.elements.name.value = name || 'Anonym';
+  refs.form.elements.message.value = message || 'Empty message';
+}
+onPageLoad();
--- src/js/03-colorpicker.js
+++ src/js/03-colorpicker.js
@@ -1,3 +1,44 @@
 import colors from './colors.json';
-import '../css/common.css';
-import '../css/colorpicker.css';
+import { loadFromLS, saveToLS } from './helpers.js';
+const refs = {
+  paletteElem: document.querySelector('.js-palette'),
+};
+
+function renderPalette(colors) {
+  const markup = colors
+    .map(({ hex, rgb }) => {
+      return `<div class="color-card">
+<div
+    class="color-swatch"
+    data-hex="${hex}"
+    data-rgb="${rgb}"
+    style="background-color: ${hex}"
+></div>
+<div class="color-meta">
+    <p>HEX:${hex}</p>
+    <p>RGB:${rgb}</p>
+</div>
+</div>`;
+    })
+    .join('');
+
+  refs.paletteElem.innerHTML = markup;
+}
+
+renderPalette(colors);
+
+refs.paletteElem.addEventListener('click', e => {
+  if (e.target === e.currentTarget) return;
+
+  const myElem = e.target.closest('.color-card').firstElementChild;
+  const color = myElem.dataset.hex;
+
+  document.body.style.backgroundColor = color;
+  saveToLS('user-color', color);
+});
+
+function onPageLoad() {
+  const color = loadFromLS('user-color') || 'white';
+  document.body.style.backgroundColor = color;
+}
+onPageLoad();
